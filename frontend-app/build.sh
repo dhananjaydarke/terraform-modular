028d29e6499ebc5f45ec7b070e817aa59e18b742
DARKE <dhananjayd@gmail.com>
2025-12-22 21:19:57 -0600

    add

--- frontend-app/build.sh
+++ frontend-app/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash  
-export API_BASE_URL="d2ppn1focjxslt.cloudfront.net"
+export API_BASE_URL="d2ppn1focjxslt.cloudfront.net/api"
 export DISTRIBUTION_ID="E1KZVRHLQMTM9R"
 export BUCKET_NAME="students-static-dev"
 API_BASE_URL=$API_BASE_URL npm run build  
