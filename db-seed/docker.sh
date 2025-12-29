a1147a99b2f586ca2ca64647a5b44ff61605eb40
DARKE <dhananjayd@gmail.com>
2025-12-29 09:40:51 -0600

    NEW

--- db-seed/docker.sh
+++ db-seed/docker.sh
@@ -9,10 +9,10 @@ export DB_USER="students_admin"
 export DB_PASSWORD="students_admin123$" 
 export DB_NAME="StudentsDB"
 export DB_HOST="students-db.calmsi4iwo2e.us-east-1.rds.amazonaws.com"
-#aws ecr create-repository --repository-name students-db-seed || true
-#aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin ${ECR_REPO_URL}
-#docker build --platform="linux/amd64" -t  $ECR_REPO_URL:$IMAGE_TAG .
-#docker push $ECR_REPO_URL:$IMAGE_TAG
+aws ecr create-repository --repository-name students-db-seed || true
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin ${ECR_REPO_URL}
+docker build --platform="linux/amd64" -t  $ECR_REPO_URL:$IMAGE_TAG .
+docker push $ECR_REPO_URL:$IMAGE_TAG
 
 
 aws ecs run-task \
--- frontend-app/build.sh
+++ frontend-app/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash  
-export API_BASE_URL="d2ppn1focjxslt.cloudfront.net/api"
-export DISTRIBUTION_ID="E1KZVRHLQMTM9R"
+export API_BASE_URL="d2uyuz7drzef9l.cloudfront.net"
+export DISTRIBUTION_ID="E2B7OJ2ZX6UMB"
 export BUCKET_NAME="students-static-dev"
 API_BASE_URL=$API_BASE_URL npm run build  
 aws s3 sync dist/ s3://$BUCKET_NAME/ --delete --cache-control max-age=300
